7a0789e18fef47418f9d12b4c357c93c5a0dcfe6
U-TANMAYA\Tanmaya <vanathiram@gmail.com>
2017-10-08 17:59:22 +1100

    version 1.2

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,6 @@
 
 function myMap() {
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+var myCenter = new google.maps.LatLng(11.074935, 77.002966);
 var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 var marker = new google.maps.Marker({position:myCenter});
